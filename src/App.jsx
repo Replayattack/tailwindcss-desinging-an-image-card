ac64ed862c965adfe750f409a702dac27d7fb37e
Daniel G <replayattack@pm.me>
2020-07-11 20:30:10 -0500

    Complete structuring a basic card lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { PropertyCard } from './PropertyCard'
 
 function App() {
+  const property = {
+    imageUrl:
+      'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+    imageAlt: 'Rear view of modern home with pool',
+    beds: 3,
+    baths: 2,
+    title: 'Modern home in city center',
+    priceInCents: 190000,
+    formattedPrice: '$1,900.00',
+    reviewCount: 34,
+    rating: 4,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-200 min-h-screen p-8 flex items-center justify-center">
+      <PropertyCard property={property} />
     </div>
   );
 }
